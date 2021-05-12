35d56738f7459cab39ddb9a43802992eedae2a1a
aleks <kornsasha89@gmail.com>
2021-05-12 14:39:03 +0300

    Lesson 8

--- src/components/CurrenciesSelect.js
+++ src/components/CurrenciesSelect.js
@@ -3,8 +3,14 @@ import {Form} from "react-bootstrap";
 
 export default function CurrenciesSelect({ currencies, selectedId = 0, onChange}) {
 
+    let options = currencies.map(item =>
+        <option key={item.id} value={item.id}>
+            {item.text}
+        </option>
+    );
+
     return <Form.Control as="select" custom value={selectedId} onChange={onChange}>
         <option value={0}>-</option>
-        {currencies.map(item => <option key={item.id} value={item.id}>{item.text}</option>)}
+        { options }
     </Form.Control>;
 }
