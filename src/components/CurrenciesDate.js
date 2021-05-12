412c346819c21e54c072eca09fae8a44534c26b0
aleks <kornsasha89@gmail.com>
2021-05-12 14:30:03 +0300

    Lesson 8

---
+++ src/components/CurrenciesDate.js
@@ -0,0 +1,6 @@
+import React from "react";
+import moment from "moment";
+
+export default function CurrenciesDate({ date, handleChange }) {
+    return <input type='date' className="form-control" value={moment(date).format('YYYY-MM-DD')} onChange={handleChange}/>
+}
