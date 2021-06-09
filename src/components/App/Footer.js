d3104905f6522ad6cff1b20efff103b78e12c351
aleks <kornsasha89@gmail.com>
2021-06-09 20:14:58 +0300

    Lesson 13

--- src/components/App/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-    return <footer className="footer">
-        <div className="container">
-            <span className="text-muted">Shop react, 2021</span>
-        </div>
-    </footer>
-}
